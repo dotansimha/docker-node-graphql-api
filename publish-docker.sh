4a04b288a4d0c5328412342a6bb8d9a3ca15236d
Dotan Simha <dotansimha@gmail.com>
2019-01-02 08:53:30 +0200

    fixes for docker script

--- publish-docker.sh
+++ publish-docker.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
 GIT_REVISION=`git log -1 --format="%H"`
-docker build -t dotansimha/docker-remote-graphql-api:$GIT_REVISION -t dotansimha/docker-remote-graphql-api:latest ./
-docker push dotansimha/docker-remote-graphql-api:$GIT_REVISION
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+
+docker build -t dotansimha/docker-remote-graphql-api:$GIT_REVISION ./
+docker tag dotansimha/docker-remote-graphql-api:$GIT_REVISION dotansimha/docker-remote-graphql-api:latest
+docker push dotansimha/docker-remote-graphql-api
